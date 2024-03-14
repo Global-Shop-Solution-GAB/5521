0e393751b53ac271ca5e898bcc27390202e882f2
Service Web <sw@gssmail.com>
2024-03-14 11:28:09 -0500

    Commit changes in C:\Websites\Git\emarlow\5521\GCG_5521_Alloc_Rec.sql

---
+++ GCG_5521_Alloc_Rec.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_5521_Alloc_Rec
+ * ========================================== */
+CREATE TABLE "GCG_5521_Alloc_Rec"(
+ "Job" CHAR(9),
+ "Suffix" CHAR(4),
+ "Seq" CHAR(4),
+ "Part" CHAR(20),
+ "Loc" CHAR(2),
+ "Serial_Number" CHAR(20),
+ "Request_Status" CHAR(15),
+ "User_Name" CHAR(8));
+
+
